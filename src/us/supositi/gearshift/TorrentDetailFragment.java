4ddaf013af2f6cb174a5ace16597b2723253a5e5
urandom <vkojouharov@gmail.com>
2013-01-05 20:39:13 +0200

    add more details. style for clickable views

--- src/us/supositi/gearshift/TorrentDetailFragment.java
+++ src/us/supositi/gearshift/TorrentDetailFragment.java
@@ -1,11 +1,16 @@
 package us.supositi.gearshift;
 
+import java.util.Arrays;
+import java.util.List;
+
 import us.supositi.gearshift.dummy.DummyContent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 /**
@@ -19,6 +24,41 @@ public class TorrentDetailFragment extends Fragment {
      * The dummy content this fragment is presenting.
      */
     private DummyContent.DummyItem mItem;
+    
+    private List<String> mPriorityValues;
+    
+    private View.OnClickListener mExpanderListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            View image;
+            View content;
+            switch(v.getId()) {
+                case R.id.torrent_detail_overview_expander:
+                    image = v.findViewById(R.id.torrent_detail_overview_expander_image);
+                    content = getView().findViewById(R.id.torrent_detail_overview_content);
+                    break;
+                case R.id.torrent_detail_limits_expander:
+                    image = v.findViewById(R.id.torrent_detail_limits_expander_image);
+                    content = getView().findViewById(R.id.torrent_detail_limits_content);
+                    break;
+                case R.id.torrent_detail_advanced_expander:
+                    image = v.findViewById(R.id.torrent_detail_advanced_expander_image);
+                    content = getView().findViewById(R.id.torrent_detail_advanced_content);
+                    break;
+                default:
+                    return;
+            }
+            
+            if (content.getVisibility() == View.GONE) {
+                content.setVisibility(View.VISIBLE);
+                image.setBackgroundResource(R.drawable.ic_section_collapse);
+            } else {
+                content.setVisibility(View.GONE);
+                image.setBackgroundResource(R.drawable.ic_section_expand);
+            }
+
+        }
+    };
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -37,6 +77,8 @@ public class TorrentDetailFragment extends Fragment {
             // to load content from a content provider.
             mItem = DummyContent.ITEM_MAP.get(getArguments().getString(TorrentDetailActivity.ARG_ITEM_ID));
         }
+        
+        mPriorityValues = Arrays.asList(getResources().getStringArray(R.array.torrent_priority_values));
     }
 
     @Override
@@ -47,44 +89,15 @@ public class TorrentDetailFragment extends Fragment {
         if (mItem != null) {
             ((TextView) root.findViewById(R.id.torrent_detail_title)).setText(mItem.content);
         }
-        
-        View.OnClickListener clickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                View image;
-                View content;
-                switch(v.getId()) {
-                    case R.id.torrent_detail_overview_expander:
-                        image = v.findViewById(R.id.torrent_detail_overview_expander_image);
-                        content = getView().findViewById(R.id.torrent_detail_overview_content);
-                        break;
-                    case R.id.torrent_detail_limits_expander:
-                        image = v.findViewById(R.id.torrent_detail_limits_expander_image);
-                        content = getView().findViewById(R.id.torrent_detail_limits_content);
-                        break;
-                    case R.id.torrent_detail_advanced_expander:
-                        image = v.findViewById(R.id.torrent_detail_advanced_expander_image);
-                        content = getView().findViewById(R.id.torrent_detail_advanced_content);
-                        break;
-                    default:
-                        return;
-                }
-                
-                if (content.getVisibility() == View.GONE) {
-                    content.setVisibility(View.VISIBLE);
-                    image.setBackgroundResource(R.drawable.ic_section_collapse);
-                } else {
-                    content.setVisibility(View.GONE);
-                    image.setBackgroundResource(R.drawable.ic_section_expand);
-                }
 
-            }
-        };
         
-        root.findViewById(R.id.torrent_detail_overview_expander).setOnClickListener(clickListener);
-        root.findViewById(R.id.torrent_detail_limits_expander).setOnClickListener(clickListener);
-        root.findViewById(R.id.torrent_detail_advanced_expander).setOnClickListener(clickListener);
-        
+        root.findViewById(R.id.torrent_detail_overview_expander).setOnClickListener(mExpanderListener);
+        root.findViewById(R.id.torrent_detail_limits_expander).setOnClickListener(mExpanderListener);
+        root.findViewById(R.id.torrent_detail_advanced_expander).setOnClickListener(mExpanderListener);
+
+        /* TODO: actually use whatever torrent priority is set */
+        ((Spinner) root.findViewById(R.id.torrent_priority)).setSelection(mPriorityValues.indexOf("normal"));
+                
         return root;
     }
 }
