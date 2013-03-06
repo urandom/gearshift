d4615f00067d32ffc3efb7b29d2dc44bccd2ca53
urandom <vkojouharov@gmail.com>
2013-03-07 00:43:20 +0200

    restore the fragment list on orientation change

--- src/org/sugr/gearshift/TorrentDetailPagerAdapter.java
+++ src/org/sugr/gearshift/TorrentDetailPagerAdapter.java
@@ -4,22 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
 public class TorrentDetailPagerAdapter extends FragmentStatePagerAdapter {
     private ArrayList<TorrentDetailPageFragment> mFragments = new ArrayList<TorrentDetailPageFragment>();
     private TransmissionSessionInterface mContext;
-  
+    private FragmentManager mFragmentManager;
+
 	public TorrentDetailPagerAdapter(FragmentActivity activity) {
 	    super(activity.getSupportFragmentManager());
-	    
+	    mFragmentManager = activity.getSupportFragmentManager();
+
 	    if (activity instanceof TransmissionSessionInterface) {
 	        mContext = (TransmissionSessionInterface) activity;
 	    }
 	}
-	
+
 	@Override
 	public int getCount() {
 		if (mContext == null) {
@@ -28,21 +32,46 @@ public class TorrentDetailPagerAdapter extends FragmentStatePagerAdapter {
 		    return mContext.getTorrents().size();
 		}
 	}
-	
+
 	@Override
 	public Fragment getItem(int position) {
 		TorrentDetailPageFragment fragment = new TorrentDetailPageFragment();
 		Bundle arguments = new Bundle();
         arguments.putInt(TorrentDetailFragment.ARG_PAGE_POSITION, position);
         fragment.setArguments(arguments);
-        
+
         while (mFragments.size() <= position)
             mFragments.add(null);
         mFragments.set(position, fragment);
-        
+
 		return fragment;
 	}
-	
+
+	@Override
+    public void restoreState(Parcelable state, ClassLoader loader) {
+        if (state != null) {
+            Bundle bundle = (Bundle)state;
+            bundle.setClassLoader(loader);
+            mFragments.clear();
+
+            Iterable<String> keys = bundle.keySet();
+            for (String key: keys) {
+                if (key.startsWith("f")) {
+                    int index = Integer.parseInt(key.substring(1));
+                    Fragment f = mFragmentManager.getFragment(bundle, key);
+                    if (f != null) {
+                        while (mFragments.size() <= index) {
+                            mFragments.add(null);
+                        }
+                        f.setMenuVisibility(false);
+                        mFragments.set(index, (TorrentDetailPageFragment) f);
+                    }
+                }
+            }
+        }
+        super.restoreState(state, loader);
+    }
+
 	public List<TorrentDetailPageFragment> getFragments() {
 	    return mFragments;
 	}
