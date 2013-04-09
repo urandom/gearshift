72486b5ebe97d55757d31b54d90fc35f36fe0a62
urandom <vkojouharov@gmail.com>
2013-04-10 00:13:50 +0300

    implement base comparator in the torrent comparator

--- src/org/sugr/gearshift/TorrentComparator.java
+++ src/org/sugr/gearshift/TorrentComparator.java
@@ -8,6 +8,7 @@ import org.sugr.gearshift.G.SortOrder;
 public class TorrentComparator implements Comparator<Torrent> {
     private SortBy mSortBy = SortBy.STATUS;
     private SortOrder mSortOrder = SortOrder.ASCENDING;
+    private SortBy mBaseSort = SortBy.AGE;
 
     public void setSortingMethod(SortBy by, SortOrder order) {
         mSortBy = by;
@@ -22,42 +23,48 @@ public class TorrentComparator implements Comparator<Torrent> {
         return mSortOrder;
     }
 
+    public void setBaseComparator(SortBy by) {
+        mBaseSort = by;
+    }
+
+    public SortBy getBaseComparator() {
+        return mBaseSort;
+    }
+
     @Override
     public int compare(Torrent a, Torrent b) {
-        /* FIXME: use addedDate instead of name as the base order */
-        String aName = a.getName(), bName = b.getName();
-        if (aName == null && bName == null) {
-            return 0;
-        } else if (aName == null) {
-            return -1;
-        } else if (bName == null) {
-            return 1;
+        int ret = compare(a, b, mSortBy, mSortOrder);
+        if (ret == 0 && mBaseSort != mSortBy) {
+            ret = compare(a, b, mBaseSort, mSortOrder);
         }
 
-        int nameComp = aName.compareToIgnoreCase(bName);
-        /* Artificially inflate the download status to appear above
-         * the seeding */
-        int statusComp = (b.getStatus() == Torrent.Status.DOWNLOADING
-            ? b.getStatus() + 10 : b.getStatus()
-        ) - (a.getStatus() == Torrent.Status.DOWNLOADING
-            ? a.getStatus() + 10 : a.getStatus());
+        return ret;
+    }
+
+    private int compare(Torrent a, Torrent b, SortBy sort, SortOrder order) {
         int ret = 0;
         float delta;
 
-        if (nameComp == 0)
-            nameComp = a.getId() - b.getId();
-        if (statusComp == 0)
-            statusComp = nameComp;
-
-        switch(mSortBy) {
+        switch(sort) {
             case NAME:
-                ret = nameComp;
+                ret = a.getName() == null && b.getName() == null
+                    ? 0 : a.getName() == null
+                    ? -1 : a.getName().compareToIgnoreCase(b.getName());
                 break;
             case SIZE:
                 ret = (int) (a.getTotalSize() - b.getTotalSize());
                 break;
             case STATUS:
-                ret = statusComp;
+                ret = (a.getStatus() == Torrent.Status.STOPPED
+                        ? a.getStatus() + 100 : a.getStatus() == Torrent.Status.CHECK_WAITING
+                        ? a.getStatus() + 10  : a.getStatus() == Torrent.Status.DOWNLOAD_WAITING
+                        ? a.getStatus() + 20  : a.getStatus() == Torrent.Status.SEED_WAITING
+                        ? a.getStatus() + 30  : a.getStatus())
+                    - (b.getStatus() == Torrent.Status.STOPPED
+                        ? b.getStatus() + 100 : b.getStatus() == Torrent.Status.CHECK_WAITING
+                        ? b.getStatus() + 10  : b.getStatus() == Torrent.Status.DOWNLOAD_WAITING
+                        ? b.getStatus() + 20  : b.getStatus() == Torrent.Status.SEED_WAITING
+                        ? b.getStatus() + 30  : b.getStatus());
                 break;
             case ACTIVITY:
                 ret = (int) ((b.getRateDownload() + b.getRateUpload()) - (a.getRateDownload() + a.getRateUpload()));
@@ -66,11 +73,9 @@ public class TorrentComparator implements Comparator<Torrent> {
                 ret = (int) (b.getAddedDate() - a.getAddedDate());
                 break;
             case LOCATION:
-                if (a.getDownloadDir() == null) {
-                    ret = -1;
-                } else {
-                    ret = a.getDownloadDir().compareToIgnoreCase(b.getDownloadDir());
-                }
+                ret = a.getDownloadDir() == null && b.getDownloadDir() == null
+                    ?  0 : a.getDownloadDir() == null
+                    ? -1 : a.getDownloadDir().compareToIgnoreCase(b.getDownloadDir());
                 break;
             case PEERS:
                 ret = a.getPeersConnected() - b.getPeersConnected();
@@ -95,11 +100,7 @@ public class TorrentComparator implements Comparator<Torrent> {
             default:
                 break;
         }
-        if (mSortBy != SortBy.NAME && mSortBy != SortBy.STATUS
-                && ret == 0) {
-            ret = statusComp;
-        }
 
-        return mSortOrder == SortOrder.ASCENDING ? ret : -ret;
+        return order == SortOrder.ASCENDING ? ret : -ret;
     }
 }
