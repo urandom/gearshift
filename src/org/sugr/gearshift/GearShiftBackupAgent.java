00ba0c36771b79104e67ca3b68fec2167b2764a7
urandom <vkojouharov@gmail.com>
2013-10-20 23:04:09 +0300

    backup seems to be working

--- src/org/sugr/gearshift/GearShiftBackupAgent.java
+++ src/org/sugr/gearshift/GearShiftBackupAgent.java
@@ -1,43 +1,38 @@
 package org.sugr.gearshift;
 
+import android.app.backup.BackupAgentHelper;
+import android.app.backup.SharedPreferencesBackupHelper;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-import android.app.backup.BackupAgentHelper;
-import android.app.backup.SharedPreferencesBackupHelper;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.preference.PreferenceManager;
-
 public class GearShiftBackupAgent extends BackupAgentHelper {
+    private static final String PREFS_BACKUP_KEY = "prefs";
+
     @Override
     public void onCreate() {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         Set<String> profile_ids = prefs.getStringSet(G.PREF_PROFILES, new HashSet<String>());
         ArrayList<String> names = new ArrayList<String>();
-        Context context = getApplicationContext();
-        PackageManager manager = context.getPackageManager();
-        PackageInfo info;
-        try {
-            info = manager.getPackageInfo(context.getPackageName(), 0);
-        } catch (NameNotFoundException e) {
-            e.printStackTrace();
-            return;
-        }
-        String packageName = info.packageName;
+        String packageName = getPackageName();
+
+        Log.d("GearShift", "#### Performing Backup!!!!");
 
         names.add(packageName + "_preferences");
+        Log.d("GearShift", "#### Scheduling " + packageName + "_preferences for backup!!!!");
+
 
         for (String id : profile_ids) {
             names.add(G.PREF_PREFIX + id);
+            Log.d("GearShift", "#### Scheduling " + G.PREF_PREFIX + id + " for backup!!!!");
         }
 
         SharedPreferencesBackupHelper helper =
             new SharedPreferencesBackupHelper(this, names.toArray(new String[names.size()]));
-        addHelper(packageName + "_backup", helper);
+        addHelper(PREFS_BACKUP_KEY, helper);
     }
 }
