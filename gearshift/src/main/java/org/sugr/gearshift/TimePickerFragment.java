3590f7abe81938d7c4e829e3f667af0721e982bc
root <root@sugr.org>
2014-02-13 23:29:25 +0200

    extract the timepickerfragment into a public class

---
+++ gearshift/src/main/java/org/sugr/gearshift/TimePickerFragment.java
@@ -0,0 +1,62 @@
+package org.sugr.gearshift;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class TimePickerFragment extends DialogFragment
+    implements TimePickerDialog.OnTimeSetListener {
+
+    public static final String ARG_HOUR = "hour";
+    public static final String ARG_MINUTE = "minute";
+    public static final String ARG_BEGIN = "begin";
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+
+        int hour, minute;
+
+        if (getArguments().containsKey(ARG_HOUR)) {
+            hour = getArguments().getInt(ARG_HOUR);
+            c.set(Calendar.HOUR_OF_DAY, hour);
+        } else {
+            hour = c.get(Calendar.HOUR_OF_DAY);
+        }
+
+        if (getArguments().containsKey(ARG_MINUTE)) {
+            minute = getArguments().getInt(ARG_MINUTE);
+            c.set(Calendar.MINUTE, minute);
+        } else {
+            minute = c.get(Calendar.MINUTE);
+        }
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+            DateFormat.is24HourFormat(getActivity()));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Button button;
+        if (getArguments().containsKey(ARG_BEGIN) && getArguments().getBoolean(ARG_BEGIN)) {
+            button = (Button) getActivity().findViewById(R.id.transmission_session_alt_limit_time_from);
+            ((TransmissionSessionActivity) getActivity()).setAltSpeedLimitTimeBegin(hourOfDay * 60 + minute);
+        } else {
+            button = (Button) getActivity().findViewById(R.id.transmission_session_alt_limit_time_to);
+            ((TransmissionSessionActivity) getActivity()).setAltSpeedLimitTimeEnd(hourOfDay * 60 + minute);
+        }
+
+        button.setText(String.format(
+            getActivity().getString(R.string.session_settings_alt_limit_time_format),
+            hourOfDay,
+            minute));
+    }
+}
--- gearshift/src/main/java/org/sugr/gearshift/TransmissionSessionActivity.java
+++ gearshift/src/main/java/org/sugr/gearshift/TransmissionSessionActivity.java
@@ -1,7 +1,5 @@
 package org.sugr.gearshift;
 
-import android.app.Dialog;
-import android.app.TimePickerDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,7 +11,6 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.LocalBroadcastManager;
 import android.text.Html;
-import android.text.format.DateFormat;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,7 +24,6 @@ import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
 import org.sugr.gearshift.datasource.DataSource;
 import org.sugr.gearshift.service.DataService;
@@ -35,7 +31,6 @@ import org.sugr.gearshift.service.DataServiceManager;
 import org.sugr.gearshift.service.DataServiceManagerInterface;
 
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
 
 public class TransmissionSessionActivity extends FragmentActivity implements DataServiceManagerInterface {
@@ -1266,57 +1261,6 @@ public class TransmissionSessionActivity extends FragmentActivity implements Dat
                 invalidateOptionsMenu();
             }
         }
-    }
-}
-
-class TimePickerFragment extends DialogFragment
-    implements TimePickerDialog.OnTimeSetListener {
-
-    public static final String ARG_HOUR = "hour";
-    public static final String ARG_MINUTE = "minute";
-    public static final String ARG_BEGIN = "begin";
-
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current time as the default values for the picker
-        final Calendar c = Calendar.getInstance();
-
-        int hour, minute;
-
-        if (getArguments().containsKey(ARG_HOUR)) {
-            hour = getArguments().getInt(ARG_HOUR);
-            c.set(Calendar.HOUR_OF_DAY, hour);
-        } else {
-            hour = c.get(Calendar.HOUR_OF_DAY);
-        }
-
-        if (getArguments().containsKey(ARG_MINUTE)) {
-            minute = getArguments().getInt(ARG_MINUTE);
-            c.set(Calendar.MINUTE, minute);
-        } else {
-            minute = c.get(Calendar.MINUTE);
-        }
-
-        // Create a new instance of TimePickerDialog and return it
-        return new TimePickerDialog(getActivity(), this, hour, minute,
-                DateFormat.is24HourFormat(getActivity()));
-    }
-
-    @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        Button button;
-        if (getArguments().containsKey(ARG_BEGIN) && getArguments().getBoolean(ARG_BEGIN)) {
-            button = (Button) getActivity().findViewById(R.id.transmission_session_alt_limit_time_from);
-            ((TransmissionSessionActivity) getActivity()).setAltSpeedLimitTimeBegin(hourOfDay * 60 + minute);
-        } else {
-            button = (Button) getActivity().findViewById(R.id.transmission_session_alt_limit_time_to);
-            ((TransmissionSessionActivity) getActivity()).setAltSpeedLimitTimeEnd(hourOfDay * 60 + minute);
-        }
-
-        button.setText(String.format(
-                getActivity().getString(R.string.session_settings_alt_limit_time_format),
-                hourOfDay,
-                minute));
     }
 }
 
