baa63a64536b80e3262f1bf302cb1cba370b4a30
Viktor Kojouharov <developer@sugr.org>
2016-05-22 23:32:02 +0300

    more log functions

--- gearshift2/src/main/kotlin/org/sugr/gearshift/C.kt
+++ gearshift2/src/main/kotlin/org/sugr/gearshift/C.kt
@@ -11,6 +11,8 @@ private object c {
     val TAG = "GearShift"
 }
 
-fun logD(message: String) {
-    Log.d(c.TAG, message)
-}
+fun logD(message: String) = Log.d(c.TAG, message)
+fun logV(message: String) = Log.v(c.TAG, message)
+fun logI(message: String) = Log.i(c.TAG, message)
+fun logW(message: String) = Log.w(c.TAG, message)
+fun logE(message: String, e: Throwable) = Log.e(c.TAG, message, e)
