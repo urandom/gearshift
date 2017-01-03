98e61d164b3b2fc0b7b4ab5099297d8d27212bff
Viktor Kojouharov <developer@sugr.org>
2017-01-03 22:05:49 +0200

    flatten the digit groups to an int

--- gearshift/src/main/kotlin/org/sugr/gearshift/viewmodel/ext/readability.kt
+++ gearshift/src/main/kotlin/org/sugr/gearshift/viewmodel/ext/readability.kt
@@ -8,11 +8,11 @@ fun Long.readableFileSize() : String {
     if (this <= 0) return "0 B";
 
     val units = arrayOf("B", "KB", "MB", "GB", "TB")
-    val digitGroups = (Math.log10(this.toDouble())/Math.log10(1024.0));
-    val scaledSize = this / Math.pow(1024.0, digitGroups);
+    val digitGroups = (Math.log10(this.toDouble())/Math.log10(1024.0)).toInt();
+    val scaledSize = this / Math.pow(1024.0, digitGroups.toDouble());
     val format = if (scaledSize < 100) "#,##0.##" else "#,##0.#"
 
-    return DecimalFormat(format).format(scaledSize) + " " + units[digitGroups.toInt()]
+    return DecimalFormat(format).format(scaledSize) + " " + units[digitGroups]
 }
 
 fun Long.readableRemainingTime(ctx : Context) : String {
