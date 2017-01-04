f89a79cf7050f899520761f867303d74fb64bd86
Viktor Kojouharov <developer@sugr.org>
2017-01-04 20:43:44 +0200

    make the bottom sheet container a nested scroll view

--- gearshift/src/main/kotlin/org/sugr/gearshift/ui/view/behaviors/CustomBottomSheetBehavior.kt
+++ gearshift/src/main/kotlin/org/sugr/gearshift/ui/view/behaviors/CustomBottomSheetBehavior.kt
@@ -5,6 +5,8 @@ import android.support.design.widget.BottomSheetBehavior
 import android.support.design.widget.CoordinatorLayout
 import android.support.v4.view.ViewCompat
 import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.VelocityTracker
 import android.view.View
 
 class CustomBottomSheetBehavior<V : View>(context: Context, attrs: AttributeSet):
